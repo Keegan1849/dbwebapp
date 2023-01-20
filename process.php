b7b2c99293dc433b95a1037c7f19e61ec41d1f67
Keegan1849 <Keeg.chapin@gmail.com>
2023-01-20 11:38:51 -0500

    fr

--- process.php
+++ process.php
@@ -1,12 +1,38 @@
 <html>
     <body>
         <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
             echo "Method: Post";
-        }else{
-            echo "Method: Get";
-        }
-        ?>
+            if(empty($_POST["firstname"])){
+                echo "No first name provided!";
+            }else{
+                echo "First Name: " .$_POST["firstname"] . "<br>";
+            }
+            if(empty($_POST["lastname"])){
+                echo "No last name provided!";
+            }else{
+                echo "Last Name: " .$_POST["lastname"]. "<br>";
+            }
+            if(empty($_POST["password"])){
+                echo "No password provided!";
+            }else{
+                echo "Password : " .$_POST["password"] . "<br>";
+            }
+            if(empty($_POST["member"])){
+                echo "Not member day";
+            }else{
+                echo "Member : " .$_POST["member"] . "<br>";
+            }
+            }else{
+            echo "Method : Get";
+            }
+
+         ?>  
+
+
     </body>
+
+
+
 
 </html>
